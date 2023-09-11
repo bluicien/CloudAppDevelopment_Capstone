8fe4fb237859304ba254c218d79f65220d0eaa5d
Brendon <bluicien@yahoo.co.nz>
2023-09-11 09:39:21 -0400

    Project progress

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
 
+    path(route='', view=views.get_dealerships, name='index'),
     # path for about view
+    path(route='about/', view=views.about, name='about'),
 
     # path for contact us view
 
@@ -19,7 +21,6 @@ urlpatterns = [
 
     # path for logout
 
-    path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
 
