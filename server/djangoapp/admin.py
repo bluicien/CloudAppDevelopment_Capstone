4b063530216bf58028f62e024d0430670e1ecfb5
Brendon <bluicien@yahoo.co.nz>
2023-09-13 07:43:14 -0400

    added car models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
 # from .models import related models
-
+from .models import CarModel, CarMake
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('model_name', 'car_type')
+    list_filter = ['car_type']
+    search_fields = ['model_name']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('make_name', 'country')
+    list_filter = ['country']
+    search_fields = ['make_name', 'country']
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -5,10 +5,15 @@ from django.utils.timezone import now
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
+class CarMake(models.Model):
+    make_name = models.CharField(null=False, max_length=100)
+    description = models.TextField()
+    country = models.CharField(max_length=50)
 # - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return "Name: " + self.make_name + "," + \
+            "Description: " + self.description + "," + \
+            "Country: " + self.country
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,9 +24,34 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'sedal'
+    SUV = 'SUV'
+    WAGON = 'wagon'
+    TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, "Wagon")
+    ]
+    dealer_id = models.IntegerField(default=0)
+    model_name = models.CharField(null=False, max_length=100)
+    car_type = models.CharField(max_length=20, choices=TYPES, default=None)
+    year = models.DateField(null=False)
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Name: " + self.model_name + "," + \
+            "Car Type: " + self.car_type
+            
+
+class CarDealer():
+    def __init__(self, name, dealer_id, dealer_address):
+        self.name = name
+        self.dealer_id = dealer_id
+        self.dealer_address = dealer_address
 
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
-
-# <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview():
+    def __init__(self, rev_title, rev_body):
+        self.rev_title = rev_title
+        self.rev_body = rev_body
